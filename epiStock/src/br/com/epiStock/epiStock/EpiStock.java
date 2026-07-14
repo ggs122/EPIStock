a0c4fe012ec18c5ccb5bb13808d1bf2ddb92f3d0
graciano <gracianogomesdasilva@gmail.com>
2026-07-14 18:00:18 -0300

    changes
    changes in epiStock class.
    
    filed product code
    
    two constructors
    
    add methods addPpeToStock and personalProtectiveEquipmentDelivery

--- epiStock/src/br/com/epiStock/epiStock/EpiStock.java
+++ epiStock/src/br/com/epiStock/epiStock/EpiStock.java
@@ -2,6 +2,7 @@ package br.com.epiStock.epiStock;
 
 public class EpiStock {
 
+    private String productCode;
     private String epiName;
     private String epiCa;
     private String epiDescriotion;
@@ -9,4 +10,29 @@ public class EpiStock {
 
     private int employeeEnrollmentNumber;
 
+    private EpiStock(String productCode, String epiName, String epiCa, String epiDescriotion, int epiAmount, int employeeEnrollmentNumber) {
+        this.productCode = productCode;
+        this.epiName = epiName;
+        this.epiCa = epiCa;
+        this.epiDescriotion = epiDescriotion;
+        this.epiAmount = epiAmount;
+        this.employeeEnrollmentNumber = employeeEnrollmentNumber;
+    }
+
+    private EpiStock(String productCode, String epiName, String epiCa, String epiDescriotion, int epiAmount) {
+        this.productCode = productCode;
+        this.epiName = epiName;
+        this.epiCa = epiCa;
+        this.epiDescriotion = epiDescriotion;
+        this.epiAmount = epiAmount;
+    }
+
+
+    public void addPpeToStock(String productCode, String epiName, String epiCa, String epiDescriotion, int epiAmount) {
+
+    }
+
+    public void personalProtectiveEquipmentDelivery() {
+
+    }
 }

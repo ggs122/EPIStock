0617168b83ab8f77c6f9e2f4a57727b386584d24
graciano <gracianogomesdasilva@gmail.com>
2026-07-14 19:07:30 -0300

    some changes

--- epiStock/src/br/com/epiStock/epiStock/EpiStock.java
+++ epiStock/src/br/com/epiStock/epiStock/EpiStock.java
@@ -1,8 +1,12 @@
 package br.com.epiStock.epiStock;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EpiStock {
 
     private int ppeId;
+    private static int ppeIdStatic = 1;
     private String ppeProductCode;
     private String ppeName;
     private String ppeCa;
@@ -30,10 +34,23 @@ public class EpiStock {
         this.ppeAmount = ppaAmount;
     }
 
+    static List<EpiStock> epiStockList = new ArrayList<>();
+
 
     public void addPpeToStock(String ppeProductCode, String ppeName, String ppeCa, String ppeDescriotion, int ppeAmountAmount) {
        boolean isPpeProductCode  = ppeProductCode.matches("Ref\\.[0-9]{2}\\.[0-9]{3}\\.[0-9]{2}");
        boolean isppeCa = ppeCa.matches("CA - ([0-9])+");
+
+       if (isPpeProductCode) {
+           if (isppeCa) {
+               EpiStock epiStock = new EpiStock(ppeIdStatic++, ppeProductCode, ppeName, ppeCa, ppeDescriotion, ppeAmount);
+               epiStockList.add(epiStock);
+           } else {
+               IO.println(String.format("Número do C.A %s -> Inválido"));
+           }
+       } else {
+           IO.println(String.format("Código do produto %s -> Inválido", ppeProductCode));
+       }
 
     }
 
